d156ad827cec8fd4ca3aeac34c129fa080c42fb1
Gerardo Barzola <gbarzolas@pucp.pe>
2017-12-17 04:22:13 +0000

    creacion de clases

---
+++ examen_final.rb
@@ -0,0 +1,19 @@
+# Programa Chef Peru quiere procesar datos de concursantes y resultados
+# Participante tiene DNI,nombre,edad,puntaje_jurado
+# Puntaje final = puntaje jurado*0.7 + puntajepublico*0.3
+# Hay chefs que son amateurs, profesionales y masters
+
+class Participante
+end
+
+class Amateur
+end
+
+class Profesional
+end
+
+class Master
+end
+
+class Administrador
+end
