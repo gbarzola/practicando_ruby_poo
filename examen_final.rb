b7c2b1c58931d9656b4d56871741a3f12545981e
Gerardo Barzola <gbarzolas@pucp.pe>
2017-12-17 04:28:01 +0000

    inicializacion de las clases

--- examen_final.rb
+++ examen_final.rb
@@ -1,18 +1,35 @@
 # Programa Chef Peru quiere procesar datos de concursantes y resultados
-# Participante tiene DNI,nombre,edad,puntaje_jurado
+# Participante tiene DNI,nombre,edad,puntaje_jurado,puntaje_publico
 # Puntaje final = puntaje jurado*0.7 + puntajepublico*0.3
 # Hay chefs que son amateurs, profesionales y masters
 
 class Participante
+    attr_accessor :dni,:nombre,:edad,:puntaje_jurado,:puntaje_publico
+    def initialize (dni,nombre,edad,puntaje_jurado,puntaje_publico)
+        @dni,@nombre,@edad,@puntaje_jurado,@puntaje_publico = dni,nombre,edad,puntaje_jurado,puntaje_publico
+    end
+    
+    def puntaje_final
+        puntaje_jurado*0.7 + puntaje_publico*0.3
+    end
 end
 
-class Amateur
+class Amateur < Participante
+    def initialize(dni,nombre,edad,puntaje_jurado,puntaje_publico)
+        super(dni,nombre,edad,puntaje_jurado,puntaje_publico)
+    end
 end
 
-class Profesional
+class Profesional < Participante
+    def initialize(dni,nombre,edad,puntaje_jurado,puntaje_publico)
+        super(dni,nombre,edad,puntaje_jurado,puntaje_publico)
+    end    
 end
 
-class Master
+class Master < Participante
+    def initialize(dni,nombre,edad,puntaje_jurado,puntaje_publico)
+        super(dni,nombre,edad,puntaje_jurado,puntaje_publico)
+    end    
 end
 
 class Administrador
