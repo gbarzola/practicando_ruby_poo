c82c26f55a4c297c15e5589871193176623a273c
Gerardo Barzola <gbarzolas@pucp.pe>
2017-12-17 05:43:34 +0000

    definicion de las operaciones de las clases

--- examen_final.rb
+++ examen_final.rb
@@ -10,7 +10,7 @@ class Participante
     end
     
     def puntaje_final
-        puntaje_jurado*0.7 + puntaje_publico*0.3
+        
     end
 end
 
@@ -18,18 +18,30 @@ class Amateur < Participante
     def initialize(dni,nombre,edad,puntaje_jurado,puntaje_publico)
         super(dni,nombre,edad,puntaje_jurado,puntaje_publico)
     end
+    def puntaje_final
+        puntaje_jurado*0.7 + puntaje_publico*0.3
+    end
 end
 
 class Profesional < Participante
-    def initialize(dni,nombre,edad,puntaje_jurado,puntaje_publico)
+    attr_accessor :anio_experiencia
+    def initialize(dni,nombre,edad,puntaje_jurado,puntaje_publico,anio_experiencia)
         super(dni,nombre,edad,puntaje_jurado,puntaje_publico)
-    end    
+        @anio_experiencia=anio_experiencia
+    end
+    def puntaje_final
+        puntaje_jurado*0.7 + puntaje_publico*0.3
+    end
 end
 
 class Master < Participante
     def initialize(dni,nombre,edad,puntaje_jurado,puntaje_publico)
         super(dni,nombre,edad,puntaje_jurado,puntaje_publico)
-    end    
+    end
+    
+    def puntaje_final
+        puntaje_jurado
+    end
 end
 
 class Administrador
