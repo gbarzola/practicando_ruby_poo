7c5e43102acfa1737b595eaf24bf0a52750823a8
Gerardo Barzola <gbarzolas@pucp.pe>
2017-12-17 06:55:06 +0000

    modificacion en insertar participante y creacion de metodo de ganador del concurso

--- examen_final.rb
+++ examen_final.rb
@@ -9,17 +9,23 @@ class Participante
         @dni,@nombre,@edad,@puntaje_jurado,@puntaje_publico = dni,nombre,edad,puntaje_jurado,puntaje_publico
     end
     
-    def puntaje_final
+    def calcular_puntaje_final
     end
+    
+    def dame_tipo_envio
+	end
 end
 
 class Amateur < Participante
     def initialize(dni,nombre,edad,puntaje_jurado,puntaje_publico)
         super(dni,nombre,edad,puntaje_jurado,puntaje_publico)
     end
-    def puntaje_final
+    def calcular_puntaje_final
         puntaje_jurado*0.7 + puntaje_publico*0.3
     end
+    def dame_tipo_envio
+        "Amateur"
+	end
 end
 
 class Profesional < Participante
@@ -28,9 +34,16 @@ class Profesional < Participante
         super(dni,nombre,edad,puntaje_jurado,puntaje_publico)
         @anio_experiencia=anio_experiencia
     end
-    def puntaje_final
-        puntaje_jurado*0.7 + puntaje_publico*0.3
+    def calcular_puntaje_final
+        if(anio_experiencia>10)
+            puntaje_jurado*0.7 + puntaje_publico*0.3 + 2
+        else
+            puntaje_jurado*0.7 + puntaje_publico*0.3
+        end
     end
+    def dame_tipo_envio
+        "Profesional"
+	end
 end
 
 class Master < Participante
@@ -38,19 +51,23 @@ class Master < Participante
         super(dni,nombre,edad,puntaje_jurado,puntaje_publico)
     end
     
-    def puntaje_final
+    def calcular_puntaje_final
         puntaje_jurado
     end
+    
+    def dame_tipo_envio
+        "Master"
+	end
 end
 
 class Factory 
     def self.crear_participante(categoria, *arg)
         case categoria
-        when "amateur"
+        when "Amateur"
             Amateur.new(arg[0],arg[1],arg[2],arg[3],arg[4])
-        when "profesional"
+        when "Profesional"
             Profesional.new(arg[0],arg[1],arg[2],arg[3],arg[4],arg[5])
-        when "master"
+        when "Master"
             Master.new(arg[0],arg[1],arg[2],arg[3],arg[4])
         end
     end
@@ -61,26 +78,35 @@ class Concurso
 	def initialize
 		@arreglo_participantes = Array.new # []
 	end
-	def registrar(participante)
-		arreglo_participantes.push(participante)
+	def registrar(categoria,*arg)
+	    envio = Factory.crear_participante(categoria,*arg)
+		arreglo_participantes.push(envio)
 	end
 	def obtener_lista_todos
 	    puts "***Listado de envios***"
         for p in arreglo_participantes
-            puts "#{p.dni} #{p.nombre} #{p.edad} #{p.puntaje_jurado} #{p.puntaje_publico} "
+            puts "#{p.dni} #{p.nombre} #{p.edad} #{p.puntaje_jurado} #{p.puntaje_publico} #{p.calcular_puntaje_final}"
         end
+	end
+	def ganador_concurso
+	    maximo_puntaje = 0
+	    ganador = []
+	    for p in arreglo_participantes
+	        if  p.calcular_puntaje_final > maximo_puntaje
+	            maximo_puntaje = p.calcular_puntaje_final
+	            ganador = p
+	        end
+	    end
+	    puts "Ganador :"
+	    puts "#{ganador.dni} #{ganador.nombre} #{ganador.edad} #{ganador.puntaje_jurado} #{ganador.puntaje_publico} #{ganador.calcular_puntaje_final}"
 	end
 end
 
 concurso = Concurso.new
-p1 = Participante.new("87665431","GBS","26",70,90)
-p2 = Participante.new("87665331","GAS","27",60,80)
-p3 = Participante.new("87665131","GDS","28",50,90)
-p4 = Participante.new("87665031","GFS","29",40,80)
-p5 = Participante.new("87665831","GGS","22",30,90)
-concurso.registrar(p1)
-concurso.registrar(p2)
-concurso.registrar(p3)
-concurso.registrar(p4)
-concurso.registrar(p5)
+concurso.registrar("Amateur","87665431","GBS","26",70,90)
+concurso.registrar("Profesional","87665331","GAS","27",60,80,11)
+concurso.registrar("Amateur","87665131","GDS","28",50,90)
+concurso.registrar("Master","87665031","GFS","29",40,80)
+concurso.registrar("Profesional","87665831","GGS","22",30,90,9)
 concurso.obtener_lista_todos
+concurso.ganador_concurso
